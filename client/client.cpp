1bf9176f7dff2fa15a3570e59134e97aec2b7cf2
keynumber <keynumber@yeah.net>
2016-02-25 22:43:35 +0800

    update

--- client/client.cpp
+++ client/client.cpp
@@ -15,16 +15,19 @@
 #include <iostream>
 #include <random>
 
+#include "server/net_complete_func.h"
+
 using namespace std;
+
 
 struct PacketHeader {
     int length;         // length为包请求内容大小,不包括包头大小
     unsigned int request_id;
+    char payload[0];
 };
 
-#define MAX_EVENTS 1000
-#define BUFFER_SIZE 1000
-#define EPOLLEN 1018
+const unsigned short server_port = 12345;
+const char * server_addr = "127.0.0.1";
 
 uint32_t suc = 0;
 uint32_t fail = 0;
@@ -35,50 +38,63 @@ int generator_sendbuf(char *buf, int len)
     int str_len = sizeof(str)/sizeof(char*);
 
     memset(buf, 0, len);
-    PacketHeader * header = (PacketHeader*)buf;
-    buf += sizeof(PacketHeader);
-    random_device rd;
-    for (int i=0; i< 3; i++) {
-        int t = rd() % str_len;
-        strcat(buf, str[t]);
-        strcat(buf, ">");
-    }
 
-    int content_len = strlen(buf);
+    PacketHeader * header = (PacketHeader*)buf;
+    random_device rd;
+    int t = rd() % str_len;
+    strcat(header->payload, str[t]);
+
+    int content_len = strlen(header->payload);
     header->length = content_len;
     header->request_id = rd();
     cout << "content_len: " << header->length << ", request_id: " << header->request_id << ", ";
-    cout << "content: " << buf << endl;
-    return sizeof(PacketHeader) + strlen(buf);
+    cout << "content: " << header->payload << endl;
+    return sizeof(PacketHeader) + content_len;
 }
 
-int write_to_server(int fd) {
-    const int buflen = 10240;
-    char buf[buflen];
-
-    int idx = 0;
-    for (int i=0; i<10; ++i) {
-        idx += generator_sendbuf(buf + idx, buflen - idx);
-    }
-
-    int send_len = 0;
-    int total_send = send_len;
+int write_to_server(int fd, char *buf, int len) {
+    int send_len = len;
+    int total_send = 0;
     while (true) {
-        cin >> send_len;
-        if (send_len < 0)
-            break;
-        cout << "offset: " << total_send << endl;
         int ret = write(fd, buf + total_send, send_len);
-        if (ret != send_len) {
-            perror("write failed");
+        if (ret < 0) {
+            perror("write to server failed, errmsg: ");
             return -1;
+        }
+
+        if (total_send == send_len) {
+            break;
         }
         total_send += send_len;
     }
     return 0;
 }
 
-int visit_client()
+int read_from_server(int clientfd, char *buf, int len)
+{
+    memset(buf, 0, len);
+
+    int packet_len = 0;
+    int total_read = 0;
+    while(true) {
+        int ret = read(clientfd, buf + total_read, len);
+        if (ret < 0) {
+            perror("read from server failed, errmsg: ");
+            return -1;
+        }
+        total_read += ret;
+        ret = ef::packet_len_func(buf, total_read, &packet_len);
+        if (ret < 0) {
+            printf("packet_len_func error\n");
+            break;
+        } else if (ret > 0){
+            return ret;
+        }
+    }
+    return -1;
+}
+
+int request_server()
 {
     struct sockaddr_in conn_addr;
     int len = sizeof(struct sockaddr_in);
@@ -90,14 +106,23 @@ int visit_client()
     }
 
     conn_addr.sin_family = AF_INET;
-    conn_addr.sin_port = htons(12345);
-    conn_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    conn_addr.sin_port = htons(server_port);
+    conn_addr.sin_addr.s_addr = inet_addr(server_addr);
     if (connect(conn, (struct sockaddr*)&conn_addr, len) < 0) {
         perror("connect error: ");
         close(conn);
         return -1;
     }
-    int ret = write_to_server(conn);
+
+    const int buflen = 10240;
+    char buf[buflen];
+    len = generator_sendbuf(buf, buflen);
+    int ret = write_to_server(conn, buf, len);
+    if (read_from_server(conn, buf, buflen) > 0) {
+        PacketHeader * header = (PacketHeader*)buf;
+        printf("read from server length %d, request_id %d, payload: %s\n",
+                header->length, header->request_id, header->payload);
+    }
     close(conn);
     return ret;
 }
@@ -105,9 +130,8 @@ int visit_client()
 int main(int argc, char * argv[])
 {
     int n = atoi(argv[1]);
-    printf("will visit server %d times\n", n);
     for (int i=0; i<n; ++i) {
-        if (visit_client() < 0) {
+        if (request_server() < 0) {
             fail++;
         } else {
             suc ++;
