470fa95b7c550fdafd35d505d6463642b324ebef
keynumber <keynumber@yeah.net>
2016-03-04 00:51:01 +0800

    update

--- common/data_struct/fix_array.h
+++ common/data_struct/fix_array.h
@@ -32,30 +32,46 @@ public:
 
     virtual ~FixArray()
     {
-        DELETE_ARRAY(_array);
-        DELETE_ARRAY(_free_list);
+        // DELETE_ARRAY(_array);
+        // DELETE_ARRAY(_free_list);
+        delete [] _array;
+        _array = nullptr;
+        delete [] _free_list;
+        _free_list = nullptr;
     }
 
     /**
      * return idx
+     * 可以通过pre将对象串成list
      */
-    inline int Push(const T & t)
+    inline int Push(int pre = -1)
     {
         if (_free_head >= 0) {
-            ++_size;
             int idx = _free_head;
             _free_head = _free_list[_free_head];
-            _array[idx] = t;
+            _free_list[idx] = -1;
+            // 没有判断pre是否大于0,直接写,造成写了指针头部之前的一些信息,
+            // 导致存在于分配指针头部的系统管理信息被覆盖,然后析构时,
+            // 碰到了奇怪的double free问题......
+            if (pre >= 0) {
+                _free_list[pre] = idx;
+            }
+            ++_size;
             return idx;
         }
         return -1;
     }
 
     /**
-     * idx: 一定是通过push得到的
+     * 只能释放整个列表
+     * idx: 一定是通过push得到的头部节点的idx
+     * 只能释放整个列表,而不能只释放部分节点
      */
-    inline int Pop(int idx) {
-        --_size;
+    inline int PopList(int idx) {
+        if (idx < 0) {
+            return 0;
+        }
+
         // _array[idx].~T();       // 执行析构函数释放对象所占用资源
         // 调用对象的析构函数,可能会释放对象的部分资源,当重新利用该对象时,即对
         // 该对象重新赋值时,则会出现SEGMENT FAULT
@@ -63,10 +79,32 @@ public:
         // 调用list的析构函数,释放list中存在的对象,不过list貌似没有把内部指针
         // 置null,当重新利用该对象时,赋值时会导致先释放原有list中的对象,此时
         // list内部包含野指针,进程就直接挂了
-        _free_list[idx] = _free_head;
+        int tail = idx;
+        int n = 1;
+        while (_free_list[tail] != -1) {
+            tail = _free_list[tail];
+            ++n;
+        }
+        _free_list[tail] = _free_head;
         _free_head = idx;
+        _size -= n;
         return 0;
     }
+
+    inline int Pop(int idx) {
+        if (idx < 0) {
+            return 0;
+        }
+
+        _free_list[idx] = _free_head;
+        _free_head = idx;
+        --_size;
+        return 0;
+    }
+
+    // idx必须是push返回的
+    // 如果是结尾返回-1,否则返回下标
+    inline int GetNext(int idx) { return _free_list[idx]; }
 
     inline T & operator[](int idx) { return _array[idx]; }
     inline int Size() const { return _size; }
