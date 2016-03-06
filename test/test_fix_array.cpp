ea88c58fd787f1fe58fbdcb0a0c989f08409d023
keynumber <keynumber@yeah.net>
2016-03-06 21:51:04 +0800

    update

--- test/test_fix_array.cpp
+++ test/test_fix_array.cpp
@@ -4,45 +4,40 @@
 
 using namespace std;
 
-class A
-{
-public:
-    A () {cout << "construct" << endl;}
-    virtual ~A () {cout << "destroy" << endl;}
-};
-
 
 void test_fix_array() {
-    int idx = 1;
+    cout << "test basic fix array" << endl;
     ef::FixArray<int> arr(5);
-    cout << "push # " << idx++  << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
-    cout << "push # " << idx++  << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
-    cout << "push # " << idx++  << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
-    cout << "push # " << idx++  << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
-    cout << "push # " << idx++  << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
-    cout << "push # " << idx++  << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
-
-
-    idx = 1;
-    cout << endl;
-    cout << "push # " << idx++  << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
-    cout << "pop # " << idx++  << " ret: " << arr.Pop(0) << " size: "; cout << arr.Size() << endl;
-    cout << "pop # " << idx++  << " ret: " << arr.Pop(1) << " size: "; cout << arr.Size() << endl;
-    cout << "push # " << idx++  << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
-    cout << "pop # " << idx++  << " ret: " << arr.Pop(2) << " size: "; cout << arr.Size() << endl;
-    cout << "pop # " << idx++  << " ret: " << arr.Pop(3) << " size: "; cout << arr.Size() << endl;
-    cout << "push # " << idx++  << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
-    cout << "pop # " << idx++  << " ret: " << arr.Pop(4) << " size: "; cout << arr.Size() << endl;
-
-    idx = 1;
-    cout << endl;
-    cout << "push # " << idx++  << " ret: " << arr.Push() << " size: " << arr.Size() << endl;
-    cout << "push # " << idx++  << " ret: " << arr.Push() << " size: " << arr.Size() << endl;
-    cout << "push # " << idx++  << " ret: " << arr.Push() << " size: " << arr.Size() << endl;
-    cout << "push # " << idx++  << " ret: " << arr.Push() << " size: " << arr.Size() << endl;
+    cout << "push # " << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
+    cout << "push # " << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
+    cout << "push # " << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
+    cout << "push # " << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
+    cout << "push # " << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
+    cout << "push # " << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
+    cout << "push # " << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
+    cout << "pop  # " << " ret: " << arr.Pop(0) << " size: "; cout << arr.Size() << endl;
+    cout << "pop  # " << " ret: " << arr.Pop(1) << " size: "; cout << arr.Size() << endl;
+    cout << "pop  # " << " ret: " << arr.Pop(2) << " size: "; cout << arr.Size() << endl;
+    cout << "pop  # " << " ret: " << arr.Pop(3) << " size: "; cout << arr.Size() << endl;
+    cout << "pop  # " << " ret: " << arr.Pop(4) << " size: "; cout << arr.Size() << endl;
+    cout << "push # " << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
+    cout << "Push # " << " ret: " << arr.Push() << " size: "; cout << arr.Size() << endl;
+    cout << "Push # " << " ret: " << arr.Push() << " size: "; cout << arr.Size() << endl;
+    cout << "push # " << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
+    cout << "pop  # " << " ret: " << arr.Pop(0) << " size: "; cout << arr.Size() << endl;
+    cout << "Push # " << " ret: " << arr.Push() << " size: "; cout << arr.Size() << endl;
+    cout << "push # " << " ret: " << arr.Push() << " size: "; cout  << arr.Size() << endl;
+    cout << "pop  # " << " ret: " << arr.Pop(0) << " size: "; cout << arr.Size() << endl;
+    cout << "pop  # " << " ret: " << arr.Pop(1) << " size: "; cout << arr.Size() << endl;
+    cout << "push # " << " ret: " << arr.Push() << " size: " << arr.Size() << endl;
+    cout << "push # " << " ret: " << arr.Push() << " size: " << arr.Size() << endl;
+    cout << "push # " << " ret: " << arr.Push() << " size: " << arr.Size() << endl;
+    cout << "push # " << " ret: " << arr.Push() << " size: " << arr.Size() << endl;
+    cout << "test basic fix array done" << endl;
 }
 
 void test_fix_array_list() {
+    cout << "test list fix array" << endl;
     int idx = 1;
     int head1, tail1;
     int head2, tail2;
@@ -51,17 +46,17 @@ void test_fix_array_list() {
     int ret = -1;
 
     ef::FixArray<int> arr(5);
-    cout << "push # " << idx++  << " ret: " << (ret = arr.Push()) << " size: "; cout  << arr.Size() << endl;
+    cout << "push # "  << " ret: " << (ret = arr.Push()) << " size: "; cout  << arr.Size() << endl;
     head1 = tail1 = ret;
-    cout << "push # " << idx++  << " ret: " << (ret = arr.Push()) << " size: "; cout  << arr.Size() << endl;
+    cout << "push # "  << " ret: " << (ret = arr.Push()) << " size: "; cout  << arr.Size() << endl;
     head2 = tail2 = ret;
-    cout << "push # " << idx++  << " ret: " << (ret = arr.Push(tail1)) << " size: "; cout  << arr.Size() << endl;
+    cout << "push # "  << " ret: " << (ret = arr.Push(tail1)) << " size: "; cout  << arr.Size() << endl;
     tail1 = ret >= 0 ? ret : tail1;
-    cout << "push # " << idx++  << " ret: " << (ret = arr.Push(tail2)) << " size: "; cout  << arr.Size() << endl;
+    cout << "push # "  << " ret: " << (ret = arr.Push(tail2)) << " size: "; cout  << arr.Size() << endl;
     tail2 = ret >= 0 ? ret : tail2;
-    cout << "push # " << idx++  << " ret: " << (ret = arr.Push(tail1)) << " size: "; cout  << arr.Size() << endl;
+    cout << "push # "  << " ret: " << (ret = arr.Push(tail1)) << " size: "; cout  << arr.Size() << endl;
     tail1 = ret >= 0 ? ret : tail1;
-    cout << "push # " << idx++  << " ret: " << (ret = arr.Push(tail2)) << " size: "; cout  << arr.Size() << endl;
+    cout << "push # "  << " ret: " << (ret = arr.Push(tail2)) << " size: "; cout  << arr.Size() << endl;
     tail2 = ret >= 0 ? ret : tail2;
 
     cout << "list1" << endl;
@@ -80,19 +75,30 @@ void test_fix_array_list() {
     }
     cout << endl;
 
-    cout << "push # " << idx++  << " ret: " << (ret = arr.Push(tail2)) << " size: "; cout  << arr.Size() << endl;
+    cout << "push # "  << " ret: " << (ret = arr.Push(tail2)) << " size: "; cout  << arr.Size() << endl;
     tail2 = ret > 0 ? ret : tail2;
+
+    cout << "pop list1" << endl;
     arr.PopList(head1);
-    cout << "push # " << idx++  << " ret: " << (ret = arr.Push(tail2)) << " size: "; cout  << arr.Size() << endl;
+
+    cout << "push # "  << " ret: " << (ret = arr.Push(-1)) << " size: "; cout  << arr.Size() << endl;
+    head1 = tail1 = ret;
+    cout << "push # "  << " ret: " << (ret = arr.Push(tail2)) << " size: "; cout  << arr.Size() << endl;
     tail2 = ret >= 0 ? ret : tail2;
-    cout << "push # " << idx++  << " ret: " << (ret = arr.Push(tail2)) << " size: "; cout  << arr.Size() << endl;
+    cout << "push # "  << " ret: " << (ret = arr.Push(tail1)) << " size: "; cout  << arr.Size() << endl;
+    tail1 = ret >= 0 ? ret : tail1;
+    cout << "push # "  << " ret: " << (ret = arr.Push(tail2)) << " size: "; cout  << arr.Size() << endl;
     tail2 = ret >= 0 ? ret : tail2;
-    cout << "push # " << idx++  << " ret: " << (ret = arr.Push(tail2)) << " size: "; cout  << arr.Size() << endl;
-    tail2 = ret >= 0 ? ret : tail2;
-    cout << "push # " << idx++  << " ret: " << (ret = arr.Push(tail2)) << " size: "; cout  << arr.Size() << endl;
-    tail2 = ret >= 0 ? ret : tail2;
-    cout << "push # " << idx++  << " ret: " << (ret = arr.Push(tail2)) << " size: "; cout  << arr.Size() << endl;
-    tail2 = ret >= 0 ? ret : tail2;
+    cout << "push # "  << " ret: " << (ret = arr.Push(tail1)) << " size: "; cout  << arr.Size() << endl;
+    tail1 = ret >= 0 ? ret : tail1;
+
+    cout << "list1" << endl;
+    t = head1;
+    while(t >= 0) {
+        cout << t << ", ";
+        t = arr.GetNext(t);
+    }
+    cout << endl;
 
     t = head2;
     cout << "list2" << endl;
@@ -101,22 +107,27 @@ void test_fix_array_list() {
         t = arr.GetNext(t);
     }
     cout << endl;
-}
 
-void test_obj()
-{
-    ef::FixArray<A> arr(10);
-    cout << "push" << endl;
-    arr.Push();
-    cout << "pop" << endl;
-    arr.Pop(0);
-    cout << "done" << endl;
+    cout << "pop list1 and list2" << endl;
+    arr.PopList(head1);
+    arr.PopList(head2);
+
+    cout << "push # "  << " ret: " << (ret = arr.Push(-1)) << " size: "; cout  << arr.Size() << endl;
+    head1 = tail1 = ret;
+    cout << "push # "  << " ret: " << (ret = arr.Push(tail2)) << " size: "; cout  << arr.Size() << endl;
+    tail2 = ret >= 0 ? ret : tail2;
+    cout << "push # "  << " ret: " << (ret = arr.Push(tail1)) << " size: "; cout  << arr.Size() << endl;
+    tail1 = ret >= 0 ? ret : tail1;
+    cout << "push # "  << " ret: " << (ret = arr.Push(tail2)) << " size: "; cout  << arr.Size() << endl;
+    tail2 = ret >= 0 ? ret : tail2;
+    cout << "push # "  << " ret: " << (ret = arr.Push(tail1)) << " size: "; cout  << arr.Size() << endl;
+    tail1 = ret >= 0 ? ret : tail1;
+    cout << "test list fix array done" << endl;
 }
 
 int main(int argc, char *argv[])
 {
     test_fix_array();
-    test_obj();
     test_fix_array_list();
     return 0;
 }
